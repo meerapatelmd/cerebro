4ecafb78212221e525ca18e7b956dc9d9283c206
Meera Patel <patelmeeray@gmail.com>
2022-01-16 00:10:27 -0500

    remove test scripts

--- R/stock.R
+++ R/stock.R
@@ -21,29 +21,3 @@ calculate_delta <-
     ) %>%
       unlist()
   }
-
-
-df %>%
-  mutate(Diff = calculate_delta(Close)) %>%
-  mutate(Date_Value = mdy_hms(Date)) %>%
-  mutate(Month = month(Date_Value,
-    label = TRUE
-  )) %>%
-  mutate(Year = year(Date_Value)) %>%
-  group_by(Month, Year) %>%
-  summarize(Month_Diff = sum(Diff)) %>%
-  ungroup() %>%
-  arrange(Month_Diff)
-
-
-df %>%
-  mutate(Diff = calculate_delta(Close)) %>%
-  mutate(Date_Value = mdy_hms(Date)) %>%
-  mutate(Month = month(Date_Value,
-    label = TRUE
-  )) %>%
-  mutate(Year = year(Date_Value)) %>%
-  dplyr::filter(
-    Month == "Dec",
-    Year == 2015
-  )
